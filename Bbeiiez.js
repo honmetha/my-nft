3af49f256f5f7782df33e8f714e959ee92a98390
Hon <dennis.mejen@gmail.com>
2020-10-12 23:08:40 +0700

    📦 NEW: Bbeiiez

---
+++ Bbeiiez.js
@@ -0,0 +1,14 @@
+// แบบกรอกเลข 4444 แต่อยากให้โชว์ในinput 4,444.00
+// พิม 3500.5 แสดง 3,500.50
+
+// Example 1:
+// Input: 4444
+// Output: "4,444.00"
+
+// Example 2:
+// Input: 3500.5
+// Output: "3,500.50"
+
+const formatPriceValue = (price) => {
+  return price;
+}
